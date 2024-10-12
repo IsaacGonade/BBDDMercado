bd3890ed6bf17f5945471996ff4083450cc7f6be
isaac <isaac.gonade@educa.jcyl.es>
2024-10-12 17:23:29 +0200

    Commit 2

--- src/main/java/org/example/bdrelacional_isaac_gonzalez/domain/Producto.java
+++ src/main/java/org/example/bdrelacional_isaac_gonzalez/domain/Producto.java
@@ -7,4 +7,72 @@ public class Producto {
     private String nombre;
     private String tipo;
 
+    public Producto() {
+    }
+
+    public Producto(int id, String tipo, String nombre, int precio, String marca) {
+        this.id = id;
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.marca = marca;
+    }
+
+    public Producto(int precio, String marca, String nombre, String tipo) {
+        this.precio = precio;
+        this.marca = marca;
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", precio=" + precio +
+                ", marca='" + marca + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", tipo='" + tipo + '\'' +
+                '}';
+    }
 }
--- src/main/java/org/example/bdrelacional_isaac_gonzalez/domain/Usuario.java
+++ src/main/java/org/example/bdrelacional_isaac_gonzalez/domain/Usuario.java
@@ -1,4 +1,28 @@
 package org.example.bdrelacional_isaac_gonzalez.domain;
 
 public class Usuario {
+
+    private String usuario;
+    private String pass;
+
+    public Usuario(String usuario, String pass) {
+        this.usuario = usuario;
+        this.pass = pass;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
 }
